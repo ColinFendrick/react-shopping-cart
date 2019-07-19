68c4a42f6bcc75c6ea747797938c790695d3b533
Colin Fendrick <colin.fendrick@gmail.com>
2019-07-19 10:12:40 -0400

    Add stubbed call test

--- cypress/integration/base.spec.js
+++ cypress/integration/base.spec.js
@@ -1,21 +1,32 @@
 describe('Testing the text filtering', () => {
-  beforeEach(() =>
-    cy.visit('/')
-  )
-  it('Filters will all, some, none', () => {
-    cy.get('ul.pokemons li.pokemons__item')
-      .should('have.length', 784)
-      .get('input[placeholder="Enter pokemon name..."]')
-      .type('saur')
-      .get('ul.pokemons li.pokemons__item')
-      .should('have.length', 3)
-      .get('input[placeholder="Enter pokemon name..."]')
-      .type('{selectall}{backspace}zzzzzzz')
-      .get('ul.pokemons li.pokemons__item')
-      .should('have.length', 0)
+  it('Sorts, adds to cart, checks out', () => {
+    const stub = cy.stub()
+
+    cy.on('window:alert', stub)
+      .visit('/')
+      .get('div.shelf-item').should('have.length', 16)
+      .get('span').contains('M').click()
+      .get('div.shelf-item').should('have.length', 1)
+      .get('div').contains('Add to cart').click()
+      .get('p.sub-price__val').should('contain', '$ 29.45')
+      .get('div').contains('Checkout')
+      .click().then(() => expect(stub.getCall(0)).to.be.calledWith('Checkout - Subtotal: $ 29.45'))
   })
 
-  it('Does stuff', () => {
-    expect(true).to.equal(true)
+  it('Does all of the above, but with a fixture!', () => {
+    // Stub our products call
+    cy.server().fixture('products').as('productsJSON')
+      .route('/products.json', '@productsJSON')
+    const stub = cy.stub()
+
+    cy.on('window:alert', stub)
+      .visit('/')
+      .get('div.shelf-item').should('have.length', 1)
+      .get('span').contains('M').click()
+      .get('div.shelf-item').should('have.length', 1)
+      .get('div').contains('Add to cart').click()
+      .get('p.sub-price__val').should('contain', '$ 666.00')
+      .get('div').contains('Checkout')
+      .click().then(() => expect(stub.getCall(0)).to.be.calledWith('Checkout - Subtotal: $ 666.00'))
   })
 })
