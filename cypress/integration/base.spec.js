b3332652fb621a6f5fdd93a6aeb39d8107c1eb09
Colin Fendrick <colin.fendrick@gmail.com>
2019-07-24 08:30:36 -0400

    Add a null test

--- cypress/integration/base.spec.js
+++ cypress/integration/base.spec.js
@@ -16,8 +16,8 @@ describe('Testing the text filtering', () => {
 
   it('Does all of the above, but with a fixture!', () => {
     // Mock our products call
-    cy.server().fixture('products').as('productsJSON')
-      .route('/products.json', '@productsJSON')
+    cy.fixture('products').as('products')
+    cy.server().route('/products.json', '@products')
     // Rest of the code is the same as above except with new result
     const stub = cy.stub()
 
@@ -30,5 +30,13 @@ describe('Testing the text filtering', () => {
       .get('p.sub-price__val').should('contain', '$ 666.00')
       .get('div').contains('Checkout')
       .click().then(() => expect(stub.getCall(0)).to.be.calledWith('Checkout - Subtotal: $ 666.00'))
+  })
+
+  it('Is ok when there\'s nothing there', () => {
+    // Mock an empty response
+    cy.server().route('/products.json', { products: []})
+
+    // Expect an empty page
+    cy.visit('/').get('span').contains('0 Product(s) found.')
   })
 })
